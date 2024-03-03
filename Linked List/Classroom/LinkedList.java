7216edb5a7d5a39af1c33601fd2c380ece0de3d7
sivakrishna <gedelashipa.1321@gmail.com>
2024-03-03 12:21:09 +0530

    Day 2 Linked List done

--- Linked List/Classroom/LinkedList.java
+++ Linked List/Classroom/LinkedList.java
@@ -60,7 +60,7 @@ public class LinkedList {
 
         Node temp = head;
         while(temp != null) {
-            System.out.print(temp.data + "-> ");
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
         System.out.println("null");
@@ -152,19 +152,139 @@ public class LinkedList {
         return -1;
     }
 
+    // Time complexity - O(n)
+    // Space complexity - O(n) --> Because of the call stack
+    public int recSearch(Node head, int key) {  
+        // Base case
+        if(head == null) {
+            return -1;
+        }
+
+        if(head.data == key) {
+            return 0;
+        }
+
+        int idx = recSearch(head.next, key);
+
+        if(idx == -1) {
+            return -1;
+        }
+        
+        return idx + 1;
+    }
+
+    // Time complexity - O(n)
+    public void reverse() { 
+        // Take 3 variables
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr != null) {
+            // Perform 4 steps
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+    }
+
+    // Time complexity - O(n)
+    public int removeNthNodeFromEnd(int n) {
+
+        if(n == size) {
+            int val = head.data;
+            head = head.next;
+            size--;
+            return val;
+        }
+
+        Node temp = head;
+        int idx = size - n;
+
+        for(int i=0; i<idx-1; i++ ) {
+            temp = temp.next;
+        }
+
+        int val = temp.next.data;
+        temp.next = temp.next.next;
+        size--;
+        return val;
+    }
+
+    public Node findMid(Node Head) {
+        Node slow = head;
+        Node fast = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next; // +1 step
+            fast = fast.next.next; // +2 step
+        }
+
+        return slow;
+    }
+
+    // Time complexity - O(n)
+    public boolean isPalindrome() {
+        // Corner case
+        if( head == null || head.next == null) {
+            return true;
+        }
+
+        // Find the mid
+        Node midNode = findMid(head);
+
+        // Reverse the half Linked List
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node right = prev;
+        Node left = head;
+
+        // Check if left half == right half
+        while(right != null) {
+            if(left.data != right.data) 
+                return false;
+            
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+
 
     public static void main(String[] args) {
       
         LinkedList ll = new LinkedList();
-        ll.addFirst(2); // O(1) time to add element to linkedlist
-        ll.addFirst(1);
-        ll.addLast(4); // O(1) time
-        ll.addLast(5);
-        ll.add(2, 3);
+        ll.addFirst(1); // O(1) time to add element to linkedlist
+        ll.addLast(2);
+        ll.addLast(97);
+        ll.addLast(97);
+        ll.addLast(2); // O(1) time
+        ll.addLast(1);
         ll.print();
 
-        System.out.println(ll.search(4));
-        System.out.println(ll.search(242));
+        // System.out.println(ll.recSearch(head,4));
+        // System.out.println(ll.recSearch(head,242));
+
+        // ll.reverse();
+        // ll.print();
+
+        // System.out.println(ll.removeNthNodeFromEnd(2));
+        // ll.print();
+        System.out.println(ll.isPalindrome());
+        
     }
     
 }
